c00f10311327ab392f67c7dce62947032f599aa5
jhpoelen <jhpoelen@xs4all.nl>
2022-09-14 10:09:24 -0500

    include upper case;

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@
 
 
 curl "http://opendata.globalnames.org/dwca/"\
- | grep -P -o "[0-9a-z-]+.(tar.gz|zip)"\
+ | grep -P -o "[0-9a-zA-Z-]+.(tar.gz|zip)"\
  | sed 's+^+http://opendata.globalnames.org/dwca/+g'\
  | xargs preston track
